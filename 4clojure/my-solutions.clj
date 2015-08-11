fa7c25e631d7b5b54988e385a96bdaa6247dfb87
Heitor Chang <heitorchang@gmail.com>
2015-08-11 17:52:38 -0300

    all 4clojure easy

--- 4clojure/my-solutions.clj
+++ 4clojure/my-solutions.clj
@@ -49,7 +49,8 @@ reduce conj ()
 ((fn foo [x]
    (when (> x 0)
      (conj (foo (dec x)) x))) 5)
-;;; when returns nil if x <= 0, and it becomes the empty list.
+;;; 'when' returns nil if x <= 0, and it becomes the empty list when used
+;;; with 'conj'.
 ;;; expression becomes (conj (conj ... (conj nil 1) 2) 3) 4) 5)
 ;;; because we are dealing with a list, the result is '(5 4 3 2 1)
 
@@ -145,3 +146,504 @@ reduce conj ()
 ;;; daowen's solution
 
 (juxt take drop)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #33 Replicate a sequence
+
+((fn [coll n] (reduce concat (map #(repeat n %) coll)))
+ [[1 2] [3 4]] 2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #39 Interleave two seqs
+
+((fn [p q] (reduce concat (map #(list % %2) p q)))
+ [1 2] [:a :b :c])
+
+;;; use mapcat: returns the result of applying concat to the result of
+;;; applying map to f and colls
+
+((fn [p q] (mapcat #(list % %2) p q))
+ [1 2] [:a :b :c])
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; #30 Compress a sequence
+
+((fn my-compress [coll]
+   (cond
+     (<= (count coll) 1) coll
+     :else (if (= (first coll) (second coll))
+             (my-compress (rest coll))
+             (conj (my-compress (rest coll)) (first coll)))))
+ [1 1 2 3 3 2 2 3])
+
+;;; daowen's solution
+#(map first (partition-by identity %))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #41 Drop every nth item
+
+((fn my-drop [coll n]
+   (concat
+    (mapcat butlast (partition n coll))
+    (drop (* n (quot (count coll) n)) coll)))
+ [1 2 3 4 5 6 7 8] 3)
+
+;;; shorter solutions use partition-all (does not throw away last elements)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #51 Advanced destructuring
+
+;;; (= [1 2 [3 4 5] [1 2 3 4 5]] ...)
+
+(let [[a b & c :as d]
+      [1 2 3 4 5]]
+  [a b c d])
+
+;;; :as d saves entire value into d. Since the last element of the test vector
+;;; is [1 2 3 4 5], it follows that d is [1 2 3 4 5], and the blank is also
+;;; [1 2 3 4 5]
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #83 A Half-truth
+
+((fn [& coll] (if (not (every? identity coll))
+                (if (some identity coll)
+                  true
+                  false)
+                false))
+ true true false)
+
+;;; use %& for collecting varargs
+;;; daowen's solution
+#(boolean (and (some true? %&) (some false? %&)))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; #118 Re-implement map
+
+;;; stack overflow
+((fn my-map [f coll]
+   (if (empty? coll)
+     ()
+     (cons (f (first coll)) (my-map f (rest coll)))))
+ inc [2 3 4 5 6])
+
+
+;;; iterative solution
+;;; times out
+((fn my-map [f coll]
+   (letfn [(iter [f coll result]
+             (if (empty? coll)
+               result
+               (recur f (rest coll) (conj result (f (first coll))))))]
+     (iter f coll [])))
+ inc [2 3 4 5 6])
+
+;;; insert recursive call in (lazy-seq ...), as suggested in Programming
+;;; Clojure 2nd ed.
+
+((fn my-map [f coll]
+   (if (empty? coll)
+     ()
+     (lazy-seq
+      (cons (f (first coll)) (my-map f (rest coll))))))
+ inc [2 3 4 5 6])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #100 Least common multiple
+
+;;; increase smallest number by single multiples until >= largest number
+;;; 1  2  3  4
+;;; 4  2  3  4
+;;; 4  4  3  4
+;;; 4  4  6  4
+;;; 8  4  6  4
+;;; 8  8  6  8
+;;; 8  8  9  8
+;;; 9  8  9  8
+;;; 9 10  9  8
+;;; 9 10  9 12
+;;;12 10  9 12
+;;;12 12 12 12
+
+;;; check that all arguments are the same
+((fn all-same [coll] (every? #(= (first coll) %) coll)) '(2 2 2))
+
+;;; increase a number by step until smallest amount at or beyond limit
+((fn increase-to [n step lim] (if (< n lim)
+                                (increase-to (+ n step) step lim)
+                                n)) 0 7 22)
+
+((fn increase-smallest-to-largest [steps vals]
+   (letfn [(increase-to [n step lim] (if (< n lim)
+                                       (increase-to (+ n step) step lim)
+                                       n))]
+     (let [smallest-index (.indexOf vals (apply min vals))
+           largest-index (.indexOf vals (apply max vals))]
+       (assoc vals smallest-index (increase-to (apply min vals)
+                                               (get steps smallest-index)
+                                               (apply max vals))))))
+ [7 3 7] [7 3 7])
+
+;;; (assoc [0 1 2 3] 2 20) => [0 1 20 3]
+
+((fn lcm [& nums]
+   (let [steps (vec nums)]
+     (letfn [(all-same [coll] (every? #(= (first coll) %) coll))
+             (increase-to [n step lim]
+               (if (< n lim)
+                 (increase-to (+ n step) step lim)
+                 n))
+             (increase-smallest-to-largest [vals]
+               (let [smallest-value (apply min vals)
+                     largest-value (apply max vals)
+                     smallest-index (.indexOf vals smallest-value)
+                     largest-index (.indexOf vals largest-value)]
+                 (assoc vals smallest-index (increase-to smallest-value
+                                                         (get steps smallest-index)
+                                                         largest-value))))
+             (iterate [result]
+               (if (all-same result)
+                 (first result)
+                 (iterate (increase-smallest-to-largest result))))]
+       (iterate steps))))
+ 7 5/7 2 3/5)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #166 Comparisons
+
+((fn my-compare [f a b]
+   (prn (f a b) (f b a)))
+ (fn [x y] (< (count x) (count y))) "pear" "plum")
+
+((fn my-compare [f a b]
+   (prn (f a b) (f b a))
+   (let [test-comparison (f a b)
+         test-opposite (f b a)]
+     (cond (= test-comparison test-opposite) :eq
+           (true? test-comparison) :lt
+           :else :gt)))
+ < 5 1)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #81 Set intersection
+
+;;; Intersection = A - (A - B)
+
+((fn [s-a s-b]
+   ;;; copied difference from source
+   (letfn [(the-diff [s1 s2] 
+             (if (< (count s1) (count s2))
+               (reduce (fn [result item] 
+                         (if (contains? s2 item) 
+                           (disj result item) 
+                           result))
+                       s1 s1)
+               (reduce disj s1 s2)))]
+     (let [s-a-minus-b (the-diff s-a s-b)]
+       (the-diff s-a s-a-minus-b))))
+ #{0 1 2 3} #{2 3 4 5})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #62 Re-implement iterate
+
+(take 5
+      ((fn my-iterate [f x]
+         (lazy-seq
+          (cons x (my-iterate f (f x)))))
+       #(* 2 %) 1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #99 Product digits
+
+#(map (comp (partial + -48) int) (seq (str (* % %2))))
+
+;;; kohyama's solution uses read-string
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #95 To Tree, or not to Tree
+
+;;; base case: single node with left and right branch
+
+((fn is-tree? [node]
+   (cond (nil? node) true
+         (not (coll? node)) false
+         (not (= (count node) 3)) false
+         :else (and (is-tree? (nth node 1)) (is-tree? (nth node 2)))))
+ '(:a (:b nil nil)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #135 Infix calculator
+
+(+ (- (/ 2 2) 48) 38)  ;; backwards
+
+(/ (- (+ 38 48) 2) 2)  ;; 42
+
+((fn infix-calc [& args]
+   (letfn [(calc-stack [args result]
+             (if (empty? args)
+               result
+               (recur (drop 2 args) ((first args) result (second args)))))]
+     (calc-stack (rest args) (first args))))
+ 20 / 2 + 2 + 4 + 8 - 6 - 10 * 9)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #122 Read a binary number
+;;;; #143 Dot Product
+
+(map (comp (partial + -48) int) (seq "1001"))
+
+((fn dot-product [v w]
+   (if (empty? v)
+     0
+     (+ (* (first v) (first w)) (dot-product (rest v) (rest w)))))
+ [2 1] [1 1])
+
+;;; better solution uses map
+#(reduce + (map * % %2))
+
+((fn powers-of-2 [n]
+   (if (= 0 n)
+     ()
+     (cons (int (Math/pow 2 (- n 1))) (powers-of-2 (- n 1)))))
+ 2)
+
+((fn read-binary [s]
+   (letfn [(str-to-seq [s]
+             (map (comp (partial + -48) int) (seq s)))
+           (dot-product [v w]
+             (if (empty? v)
+               0
+               (+ (* (first v) (first w)) (dot-product (rest v) (rest w)))))
+           (powers-of-2 [n]
+             (if (= 0 n)
+               ()
+               (cons (int (Math/pow 2 (- n 1))) (powers-of-2 (- n 1)))))]
+     (let [s-len (count s)]
+       (dot-product (powers-of-2 s-len) (str-to-seq s)))))
+ "11")
+
+;;; dankolov's solution
+(fn read-binary [s]
+  (->> s
+       (map #(- (int %) (int \0)))  ;; convert to array
+       (reduce #(+ (* 2 %1) %2))))  ;; during each step of the reduction,
+;;; accumulated values are repeatedly multiplied by 2
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #90 Cartesian product
+
+((fn [a b]
+   (set
+    (for [x a, y b]
+      [x y])))
+ #{"A" "K" "Q"} #{"S" "H" "D" "C"})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #128 Recognize playing cards
+
+((fn [s]
+   (let [suits {"C" :club, "D" :diamond, "H" :heart, "S" :spade}
+         ranks {"2" 0, "3" 1, "4" 2, "5" 3, "6" 4, "7" 5, "8" 6,
+                "9" 7, "T" 8, "J" 9, "Q" 10, "K" 11, "A" 12}
+         s-chars (seq s)
+         suit-str (str (first s-chars))
+         rank-str (str (second s-chars))]
+     (hash-map :suit (suits suit-str) :rank (ranks rank-str))))
+ "CA")
+
+;;; better solutions destructure the input string to first and second chars
+
+((fn smaller-than-sum-square-digits? [n]
+   (let [digits-array (map #(- (int %) (int \0)) (str n))] ;; convert to array
+     (< n (reduce + (map #(* % %) digits-array)))))
+ 19)
+
+((fn count-smaller-than-sum-sq [coll]
+   (letfn [(smaller-than-sum-square-digits? [n]
+             (let [digits-array (map #(- (int %) (int \0)) (str n))]
+               (< n (reduce + (map #(* % %) digits-array)))))]
+     (count (filter smaller-than-sum-square-digits? coll))))
+ (range 10))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #96 Beauty is symmetry
+
+((fn [node]
+   (letfn [(expand [node]
+             (if (nil? node)
+               nil
+               (list (expand (nth node 1))
+                     (first node)
+                     (expand (nth node 2)))))]
+     (let [flat-tree (flatten (expand node))]
+       (= flat-tree (reverse flat-tree)))))
+ '(:a (:b nil nil) (:c nil nil)))
+
+((fn flip [node]
+   (list (first node) (nth node 2) (nth node 1)))
+ '(:a :b :c))
+
+(flatten ((fn expand [node]
+            (if (nil? node)
+              nil
+              (list (expand (nth node 1)) (first node) (expand (nth node 2)))))
+          '(:a (:b nil nil) (:c nil nil))))
+
+;;; cleaner solution destructures
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #97 Pascal's triangle
+
+;;; add 0 to left of current vector and 0 to the right of another copy, then
+;;; (map + [1 2 1 0] [0 1 2 1])
+
+((fn pascal-tri [n]
+   (if (= n 1)
+     [1]
+     (let [prev-line (pascal-tri (- n 1))]
+       (map + (concat [0] prev-line) (concat prev-line [0])))))
+ 5)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #147 Pascal's trapezoid
+
+(take 4
+      ((fn pascal-trapezoid [line]
+         (lazy-seq
+          (cons line (pascal-trapezoid (map +' (concat [0] line) (concat line [0]))))))
+       [1]))
+;;; note: using +' to avoid overflow in the last test case
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #63 Group a sequence
+
+;;;; list of results for each input
+((fn [f inputs]
+   (interleave (map f inputs) inputs))
+ #(> % 5) [1 3 6 8])
+
+;;;; (assoc {true [] false []} true (conj ({true [] false []} true) 1))
+
+;;;; make a set with unique results, and assign [] to each
+((fn group-a-seq [f inputs]
+   (let [results (map f inputs)
+         unique-results (set results)
+         interleaved-results (interleave results inputs)
+         grouped (apply hash-map (interleave unique-results (repeat [])))]
+     (letfn [(accum [interl result]
+               (if (empty? interl)
+                 result
+                 (accum (drop 2 interl)
+                        ;;; result)))]
+                        (assoc result (first interl)
+                               (conj (result (first interl)) (second interl))))))]
+       (prn grouped)
+       (accum interleaved-results grouped))))
+ #(> % 5) [1 3 6 8])
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #146 Trees into tables
+
+;;; use for (?)
+;;; instead of picking two levels of a hashmap, unite the first two levels
+;;; into a vector, that would lead to the desired value.
+
+((fn [m]
+   ))
+
+(def q146 '{a {p 1, q 2}, b {m 3, n 4}})
+
+;;; correct, but paths should not be inside a vector
+(for [[k1 v1] q146 [k2 v2] v1]
+  [[k1 k2] v2])
+
+(apply hash-map (apply concat
+                       (for [[k1 v1] q146 [k2 v2] v1]
+                         [[k1 k2] v2])))
+
+;;; flatten one level
+((fn [c]
+   (apply concat c))
+ '[[1 2] [3 [4]] [5]])
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #153 Pairwise disjoint sets
+
+
+((fn [s]
+   (let [elts (apply concat s)]
+     (= (count elts) (count (set elts)))))
+ #{#{1} #{2}})
+
+
+((fn [s]
+   (let [elts (apply concat s)]
+     (apply distinct? elts)))
+ #{#{1} #{2}})
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #44 Rotate sequence
+
+;;; mod with (count...)
+
+;;; rotate 1 moves items left
+
+((fn [n s]
+   (letfn [(rotate-by-one [n s]
+             (if (= n 0)
+               s
+               (recur (- n 1) (conj (vec (rest s)) (first s)))))]
+     (rotate-by-one (mod n (count s)) s)))
+ 1 '(:a :b :c))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #43 Reverse interleave
+
+;;; apply nth to iterate
+
+(def q43 (range 9))
+
+(map #(nth q43 % 0) (take 3 (iterate #(+ 3 %) 0)))
+
+((fn [s subs]
+   (let [ct (count s)
+         sublen (quot ct subs)]
+     (letfn [(build-subs [i result]
+               (if (= i subs)
+                 result
+                 (recur (+ i 1) (conj result (map #(nth s % 0) (take sublen (iterate #(+ subs %) i)))))))]
+       (build-subs 0 []))))
+ [1 2 3 4 5 6] 2)
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;; #55 Count occurrences
+
+(fn [s] (apply hash-map
+               (mapcat #(list (first %) (count %))
+                       (partition-by identity (sort s)))))
+
+;;; hisba's solution
+((fn [s]
+   (into {}
+         (map #(vector (key %) (count (val %)))
+              (group-by identity s))))
+ '(1 1 2 3 1 1 1 2 3 3 3 3 3))
+
+;;;; how does (into {} ... ) work?
+(#(into {} (for [k %2] [k %])) 0 [:a :b])
+
+(for [k [:a :b]] [k 0])
--- scratch/scratch.clj
+++ scratch/scratch.clj
@@ -20,3 +20,12 @@
     2)
   
   )
+
+
+(time
+ (nth
+  ((fn my-lazy-range [n]
+     (lazy-seq 
+      (cons n (my-lazy-range (inc n)))))
+   0) 999999))
+
